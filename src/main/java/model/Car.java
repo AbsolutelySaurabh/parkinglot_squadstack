35457f203dd697c1ea3493cf023fd218257d0de6
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2020-10-02 15:53:10 +0530

    added vehicle model

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * @author AbsolutelySaurabh
+ */
+public class Car extends Vehicle {
+
+    public Car(String registrationNo, int driverAge) {
+        super(registrationNo, driverAge);
+    }
+}
---
+++ src/main/java/model/Vehicle.java
@@ -0,0 +1,33 @@
+package model;
+
+/**
+ * @author AbsolutelySaurabh
+ */
+public abstract class Vehicle {
+
+    private String registrationNo;
+    private int driverAge;
+
+    public Vehicle(String registrationNo, int driverAge) {
+        this.registrationNo = registrationNo;
+        this.driverAge = driverAge;
+    }
+
+    @Override
+    public String toString() {
+        return "[registrationNo=" + registrationNo + ", driverAge=" + driverAge + "]";
+    }
+
+    public String getRegistrationNo() {
+        return registrationNo;
+    }
+    public void setRegistrationNo(String registrationNo) {
+        this.registrationNo = registrationNo;
+    }
+    public int getDriverAge() {
+        return driverAge;
+    }
+    public void setDriverAge(int driverAge) {
+        this.driverAge = driverAge;
+    }
+}
